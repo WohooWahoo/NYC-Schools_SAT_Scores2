4432b0ab0f9ce6f468067fcf70c24a93b4d26630
WohooWahoo <8032739+WohooWahoo@users.noreply.github.com>
2023-08-14 03:16:56 -0500

    Update SchoolDetailCell.swift

--- NYC-Schools_SAT_Scores2/NYC-Schools_SAT_Scores/NYC-Schools_SAT_Scores/SchoolDetailCell.swift
+++ NYC-Schools_SAT_Scores2/NYC-Schools_SAT_Scores/NYC-Schools_SAT_Scores/SchoolDetailCell.swift
@@ -19,25 +19,15 @@ class SchoolDetailCell: UITableViewCell {
         return SchNameLbl
     }()
     
-//    public var schoolImage: UIImageView = {
-//        let image = UIImageView()
-//        image.image = UIImage(named: "School.png")
-//        image.contentMode = .scaleAspectFill
-//        image.clipsToBounds = true
-//        return image
-//    }()
-    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-            super.init(style: style, reuseIdentifier: reuseIdentifier)
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
           contentView.backgroundColor = .white
           contentView.addSubview(schoolNameLabel)
-//        contentView.addSubview(schoolImage)
         commonInit()
         }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
-        //fatalError("init(coder:) has not been implemented")
         commonInit()
     }
     
@@ -57,14 +47,4 @@ class SchoolDetailCell: UITableViewCell {
         ])
     }
 
-//    private func setupSchoolImageConstraint() {
-//        addSubview(schoolImage)
-//        schoolImage.translatesAutoresizingMaskIntoConstraints = false
-//        NSLayoutConstraint.activate([
-//            schoolImage.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
-//            schoolImage.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8),
-//            schoolImage.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8),
-//            schoolImage.widthAnchor.constraint(equalTo: safeAreaLayoutGuide.widthAnchor, multiplier: 0.20)
-//        ])
-//    }
 }
